5c55dada1cec2a86bb00e0edff34c91f93270e51
drlussac <vgshina@gmail.com>
2022-06-27 00:01:55 +0530

    inital commit

---
+++ app/main.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI
+from . import models
+from .database import engine
+from .routers import post, users, auth, vote
+from .config import settings
+from fastapi.middleware.cors import CORSMiddleware
+
+# models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(post.router)
+app.include_router(users.router)
+app.include_router(auth.router)
+app.include_router(vote.router)
+
+
+@app.get("/")
+def root():
+    return {"message": "welecome to my api"}
+
+
+# @app.get("/sqlalchemy")
+# def test_posts(db: Session = Depends(get_db)):
+#     posts = db.query(models.Post).all()
+#     return {"data": posts}
